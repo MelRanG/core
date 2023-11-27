220c27c1354257dd5328fd0f8bdcd4998d1f0a89
MelRanG <tpdud285@gmail.com>
2023-11-27 10:47:40 +0900

    [ADD] 컴포넌트 스캔 위치 지정과 컴포넌트 스캔에 filter를 넣어서 탐색 대상에 제외, 추가하는 방법 및 자체 어노테이션 생성하는 법

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -8,5 +8,6 @@ import org.springframework.context.annotation.FilterType;
 @ComponentScan(
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )//기존에 AppConfig는 제외하기 위해서 위 설정을 사용한다.
+//컴포넌트 스캔은 최상단에 넣는 것이 좋고 @SpringBootApplication 안에 컴포넌트 스캔이 포함돼있다.
 public class AutoAppConfig {
 }
