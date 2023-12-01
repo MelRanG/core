bb2bac28b4af24757b0a00ad6d3f71dea813cfcb
patrick <tpdud285@gmail.com>
2023-12-01 21:14:55 +0900

    [ADD] 어노테이션을 만들어서 Qualifier를 사용할 때 컴파일 단계에서 에러체크 가능하게 추가

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -1,14 +1,14 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
 //@Qualifier("mainDiscountPolicy") //중복등록됐을 때 구분하게한다. 사용하는쪽에서도 이름을 지정해야한다.
-@Primary //중복등록됐을 때 @Primary가 있으면 최우선으로 가져온다.
+//@Primary //중복등록됐을 때 @Primary가 있으면 최우선으로 가져온다.
+@MainDiscountPolicy //어노테이션을 붙이면 컴파일단계에서 오류를 찾는다. Qualifier는 문자열이라 컴파일단계에서 찾지 못한다.
 public class FixDiscountPolicy implements DiscountPolicy{
 
     private int discountFixAmount = 1000;
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
@@ -7,13 +8,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     //배우는 연기만 해야지 배역을 고르면 안된다. 배역을 고르고, 조명을 세팅하는 건 별도의 공연기획자가 해야한다.
     //OrderServiceImpl은 구현체를 뭘로 할지 선택하면 안된다.
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
