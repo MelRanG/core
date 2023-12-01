5d297bed9eccb68d8986bd586572fad20b0b2fa7
patrick <tpdud285@gmail.com>
2023-12-01 20:32:59 +0900

    [ADD] 롬복 추가

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,18 +3,17 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     //배우는 연기만 해야지 배역을 고르면 안된다. 배역을 고르고, 조명을 세팅하는 건 별도의 공연기획자가 해야한다.
     //OrderServiceImpl은 구현체를 뭘로 할지 선택하면 안된다.
     private final MemberRepository memberRepository;
     private DiscountPolicy discountPolicy;
-
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
