0726e3857190b4641bb310c94a0399eac5e440cd
MelRanG <tpdud285@gmail.com>
2023-12-04 16:21:59 +0900

    [ADD] NetworkClient 세팅

---
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,30 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest(){
+        //ApplicationContext에는 close가 없어서 그 하위인 Configure을 사용한다.
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig{
+        @Bean
+        public NetworkClient networkClient(){
+            NetworkClient networkClient = new NetworkClient();
+            //생성자를 만들 때 connect와 url을 세팅해서 null이 뜬다.
+            networkClient.setUrl("http://hello.dev");
+            return networkClient;
+        }
+    }
+}
