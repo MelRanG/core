94764fbf6d70dab4ab8b69e4ab0207d739266c99
patrick <tpdud285@gmail.com>
2023-12-08 23:56:59 +0900

    [ADD] Provider를 사용하여 Request스코프를 사용할 때 의존성이 없는 문제 해결. Provider는 의존성이 주입될 때 DL로 주입되기 때문에 주입을 먼저 하고 사용할 때 새로운 객체를 생성해서 문제를 해결한다.

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,12 +13,16 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    //MyLogger를 찾을 수 있는 DL이 생성시접에 주입됨. 처음 생성할 때는 Provider로 받고 HTTP가 살아있을 때(logDemo가 호출될 때) 의존성 주입이 발생함
+    //즉 처음에 의존성주입을 해야하는데 MyLogger는 request 스코프니 주입이 안돼서 provider로 대신 받음
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request){
+        //호출하면 myLogger를 찾아서 주입함.
         String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
