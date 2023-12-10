d0ab1e1822f7c5b534a27ac316308e0a4ea2cd0e
patrick <tpdud285@gmail.com>
2023-12-10 16:58:01 +0900

    [ADD] proxyMode를 사용해서 requestScope문제 해결. proxyMode는 CGLIB처럼 의존성 주입 시점에 가짜 프록시 객체를 만들고 해당 빈을 실제로 사용할 때 실제 빈을 생성한다.

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package hello.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,7 +9,9 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value = "request")
+//의존성 주입 시점에 가짜 Proxy객체를 만들어서 주입시킨다. 실제 객체를 호출할 때 실제 빈을 호출한다. 가짜 프록시 객체는 원본 객체를 상속받았기 때문에 클라이언트는 원본인지 모르고 사용 가능
+//가짜 객체는 scope와 전혀 관련이 없다. 어차피 호출이 발생하면 가짜가 새로운 빈을 주입하기 떄문. 그래서 가짜는 싱글톤처럼 동작한다.
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class MyLogger {
 
     private String uuid;
--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -13,16 +13,16 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    //MyLogger를 찾을 수 있는 DL이 생성시접에 주입됨. 처음 생성할 때는 Provider로 받고 HTTP가 살아있을 때(logDemo가 호출될 때) 의존성 주입이 발생함
+    //MyLogger를 찾을 수 있는 DL이 생성시점에 주입됨. 처음 생성할 때는 Provider로 받고 HTTP가 살아있을 때(logDemo가 호출될 때) 의존성 주입이 발생함
     //즉 처음에 의존성주입을 해야하는데 MyLogger는 request 스코프니 주입이 안돼서 provider로 대신 받음
-    private final ObjectProvider<MyLogger> myLoggerProvider;
-
+//    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request){
         //호출하면 myLogger를 찾아서 주입함.
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
+//        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
